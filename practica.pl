8b8daaa9707ef54ec85e7a6cf9dc021a7e836ed3
Jaume <jaumemesquida259@gmail.com>
2021-05-31 12:34:43 +0200

    Diferentss

--- practica.pl
+++ practica.pl
@@ -28,10 +28,6 @@ imprimir(I1,I2,F2,F3,F4,F1).
 imprimirLinea([]).
 imprimirLinea([X|L]):- write(X), write(' '), imprimirLinea(L).
 
-/* Funció per anomenar els elements de la matriu */
-anomenarElements([],_,_,_,_).
-anomenarElements([X|F1],E1,E2,E3,E4):- E1 = X, anomenarElements(F1,E2,E3,E4,E1).
-
 /* Funció per comprovar si els valors del cantons son els correctes */
 comprovarValorCantons(X, X, X).
 comprovarValorCantons('-', _, _).
@@ -49,19 +45,44 @@ PROGRAMA PRINCIPAL
 
 lletres(M1,M2,M3,M4) :- reverse(M3,I3), reverse(M4,I4), 
 
-anomenarElements(M1,M11,M12,M13,M14), 
-anomenarElements(M2,M21,M22,M23,M24), 
-anomenarElements(I3,M31,M32,M33,M34), 
-anomenarElements(I4,M41,M42,M43,M44), 
+M1 = [M11,M12,M13,M14],
+M2 = [M21,M22,M23,M24],
+I3 = [M31,M32,M33,M34],
+I4 = [M41,M42,M43,M44],
 
 permutacio(['a','b','c','-'],F1),
-anomenarElements(F1,C11,C12,C13,C14), 
+F1 = [C11,C12,C13,C14],
+
 permutacio(['a','b','c','-'],F2),
-anomenarElements(F2,C21,C22,C23,C24),
+F2 = [C21,C22,C23,C24],
+
+
+C1v1 = [C11,C21], 
+C2v1 = [C12,C22],
+C3v1 = [C13,C23],
+C4v1 = [C14,C24], 
+
+diferents(C1v1),
+diferents(C2v1),
+diferents(C3v1),
+diferents(C4v1),
+
 permutacio(['a','b','c','-'],F3), 
-anomenarElements(F3,C31,C32,C33,C34),
+F3 = [C31,C32,C33,C34],
+
+
+C1v2 = [C11,C21,C31], 
+C2v2 = [C12,C22,C32],
+C3v2 = [C13,C23,C33],
+C4v2 = [C14,C24,C34], 
+
+diferents(C1v2),
+diferents(C2v2),
+diferents(C3v2),
+diferents(C4v2),
+
 permutacio(['a','b','c','-'],F4),
-anomenarElements(F4,C41,C42,C43,C44),
+F4 = [C41,C42,C43,C44],
 
 C1 = [C11,C21,C31,C41], 
 C2 = [C12,C22,C32,C42],
@@ -88,31 +109,12 @@ comprovarValorLaterals(C21, M42),
 comprovarValorLaterals(C31, M43),
 
 write('   '), imprimirLinea(M1),write('\n'),
-imprimir(M2,I4,F1,F2,F3,F4), write('   '), imprimirLinea(I3).
+imprimir(M2,I4,F1,F2,F3,F4), write('   '), imprimirLinea(I3), fail.
 
 
 /* Funció comptar */
 
-comptar :- asserta(comptador(0)), 
-
-permutacio(['a','b','c','-'],F1),
-anomenarElements(F1,C11,C12,C13,C14), 
-permutacio(['a','b','c','-'],F2),
-anomenarElements(F2,C21,C22,C23,C24),
-permutacio(['a','b','c','-'],F3), 
-anomenarElements(F3,C31,C32,C33,C34),
-permutacio(['a','b','c','-'],F4),
-anomenarElements(F4,C41,C42,C43,C44),
-
-C1 = [C11,C21,C31,C41], 
-C2 = [C12,C22,C32,C42],
-C3 = [C13,C23,C33,C43],
-C4 = [C14,C24,C34,C44], 
-
-diferents(C1),
-diferents(C2),
-diferents(C3),
-diferents(C4),
+comptar :- asserta(comptador(0)), lletres(_,_,_,_),
 
 comptador(X), X1 is X+1, 
 retract(comptador(X)), 
